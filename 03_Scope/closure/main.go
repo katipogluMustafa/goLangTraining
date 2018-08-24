ef301b3e93333e0c79d5274e49db05f948bd04fb
Mustafa Katipoğlu <mustafakatipoglu98@hotmail.com>
2018-08-24 22:59:30 +0300

    update disclosure

--- 03_Scope/closure/main.go
+++ 03_Scope/closure/main.go
@@ -2,14 +2,18 @@ package main
 
 import "fmt"
 
+var k = "some string"
+
 func main(){
+
+	/* Anything declared in the outer scope is accessible in scope that are enclosed by that outer scope */
 
 	x := 42
 	fmt.Println(x)
-
+	fmt.Println(k)//k is declared outer scope, it is visible in this enclosed scope
 	{
 
-		fmt.Println(x)//x is visible since x is declared at outer scope
+		fmt.Println(x)//x is visible since x is declared at outer scope and outer scope enclosing inner scope
 		y := "Some text"
 		fmt.Println(y)//y is only visible inside of the braces
 
