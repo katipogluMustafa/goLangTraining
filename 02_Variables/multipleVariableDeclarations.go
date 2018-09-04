61de6de603d488b02dc7d7ed63d4f0fc21fcbe4a
Mustafa Katipoğlu <mustafakatipoglu98@hotmail.com>
2018-09-04 18:04:22 +0300

    update variables

---
+++ 02_Variables/multipleVariableDeclarations.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+	var variable1,variable2,variable3 string
+	var variable4,variable5,variable6 int = 4,5,6
+	var variable7,variable8,variable9 = 7,8,9
+	variable10,variable11,variable12 := 10,11,12
+
+	for i := 1; i < 12; i++ {
+		s := fmt.Sprint("%s%d","variable",i)
+		fmt.Println(s)
+	}
+
+	fmt.Println(variable1,variable2,variable3)
+	fmt.Println(variable4,variable5,variable6)
+	fmt.Println(variable7,variable8,variable9)
+	fmt.Println(variable10,variable11,variable12)
+}
