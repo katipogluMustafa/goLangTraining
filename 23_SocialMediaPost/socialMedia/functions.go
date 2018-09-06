e616577d1bb2ad9438a58614f8ee6bd6f9cd175c
Mustafa Katipoğlu <mustafakatipoglu98@hotmail.com>
2018-09-06 16:54:56 +0300

    update

--- 23_SocialMediaPost/socialMedia/functions.go
+++ 23_SocialMediaPost/socialMedia/functions.go
@@ -4,6 +4,8 @@ import (
 	"time"
 )
 
+//go:generate stringer -type=MoodState
+
 type MoodState int
 
 // define all the possible mood states using an iota enumerator.
@@ -21,46 +23,47 @@ const (
 )
 
 type AuditableContent struct {
-	TimeCreated		time.Time
-	TimeModified	time.Time
-	CreatedBy		string
-	ModifiedBy		string
+	TimeCreated  time.Time
+	TimeModified time.Time
+	CreatedBy    string
+	ModifiedBy   string
 }
 
 // Social Media Post
 type Post struct {
 	AuditableContent // Embedded type
-	Caption		 	string
-	MessageBody		string
-	URL				string
-	ImageURI		string
-	ThumbnailURI	string
-	Keywords		[]string
-	Likers			[]string
-	AuthorMood		MoodState
+	Caption          string
+	MessageBody      string
+	URL              string
+	ImageURI         string
+	ThumbnailURI     string
+	Keywords         []string
+	Likers           []string
+	AuthorMood       MoodState
 }
+
 // we use map to remember moodstates
 var Moods map[string]MoodState
 
 // init() responsible for initializing the mood state
 func init() {
 	Moods = map[string]MoodState{
-		"neutral": MoodStateNeutral,
-		"happy": MoodStateHappy,
-		"sad": MoodStateSad,
-		"angry": MoodStateAngry,
-		"hopeful": MoodStateHopeful,
-		"thrilled": MoodStateThrilled,
-		"bored": MoodStateBored,
-		"shy": MoodStateShy,
-		"comical": MoodStateComical,
+		"neutral":   MoodStateNeutral,
+		"happy":     MoodStateHappy,
+		"sad":       MoodStateSad,
+		"angry":     MoodStateAngry,
+		"hopeful":   MoodStateHopeful,
+		"thrilled":  MoodStateThrilled,
+		"bored":     MoodStateBored,
+		"shy":       MoodStateShy,
+		"comical":   MoodStateComical,
 		"cloudnine": MoodStateOnCloudnine,
 	}
 }
 
 // constructor for new social media post
 
-func NewPost(username string, mood MoodState, caption string, messageBody string, url string, imageURI string, thumbnailURI string, keywords []string ) *Post{
-	auditableContent := AuditableContent{CreatedBy:username, TimeCreated:time.Now()}
-	return &Post{Caption: caption, MessageBody:messageBody, URL:url, ImageURI:imageURI, ThumbnailURI:thumbnailURI, AuthorMood:mood, Keywords:keywords, AuditableContent: auditableContent}
+func NewPost(username string, mood MoodState, caption string, messageBody string, url string, imageURI string, thumbnailURI string, keywords []string) *Post {
+	auditableContent := AuditableContent{CreatedBy: username, TimeCreated: time.Now()}
+	return &Post{Caption: caption, MessageBody: messageBody, URL: url, ImageURI: imageURI, ThumbnailURI: thumbnailURI, AuthorMood: mood, Keywords: keywords, AuditableContent: auditableContent}
 }
--- 24_ConcurrentlyComputePi/main.go
+++ 24_ConcurrentlyComputePi/main.go
@@ -19,14 +19,15 @@ const ANSISecondSlotScreenSequence = "\033[3;0H"
 var pichan = make(chan float64)
 
 var computationDone = make(chan bool, 1)
+
 // Number of Nilakantha terms for the scoreboard
 var termsCount int
 
 /* Virtual Scoreboard*/
-func printCalculationSummary(){
-	fmt.Print(/* ANSIClearScreenSequence */)
-	fmt.Println(/* ANSFirstSlotScreenSequence ,*/ "Computed value of Pi:\t\t", <-pichan)
-	fmt.Println(/* ANSISecondSlotScreenSequence,*/ "# of Nilakantha Terms:\t\t", termsCount)
+func printCalculationSummary() {
+	fmt.Print( /* ANSIClearScreenSequence */ )
+	fmt.Println( /* ANSFirstSlotScreenSequence ,*/ "Computed value of Pi:\t\t", <-pichan)
+	fmt.Println( /* ANSISecondSlotScreenSequence,*/ "# of Nilakantha Terms:\t\t", termsCount)
 }
 
 func pi(n int) float64 {
@@ -38,10 +39,10 @@ func pi(n int) float64 {
 	f := 3.0
 
 	// Sump up the calculated Nilakantha terms for n steps
-	for k:= 1; k <= n; k++ {
+	for k := 1; k <= n; k++ {
 		termsCount++
-		 f += <-ch
-		pichan <-f
+		f += <-ch
+		pichan <- f
 	}
 
 	// notify that the computation is done over the channel
@@ -49,29 +50,29 @@ func pi(n int) float64 {
 	return f
 }
 
-func nilakanthaTerm(ch chan float64, k float64){
+func nilakanthaTerm(ch chan float64, k float64) {
 	j := 2 * k
-	if int64(k) % 2 == 1 {
-		ch <- 4.0 / ( j * (j + 1) * (j + 2) )
-	}else {
-		ch <- -4.0 / ( j * (j + 1) * (j + 2) )
+	if int64(k)%2 == 1 {
+		ch <- 4.0 / (j * (j + 1) * (j + 2))
+	} else {
+		ch <- -4.0 / (j * (j + 1) * (j + 2))
 	}
 }
 
-func main(){
+func main() {
 	ticker := time.NewTicker(time.Millisecond * 108)
 
 	/*
 	 * If the user wants to prematurely break out of the program by issuing a Ctrl+C
 	 * 	 we tell the signal package to notify us over this interrupt channel
-	*/
+	 */
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 
 	go pi(5000)
 
 	/* update values on the scoreboard */
-	go func(){
+	go func() {
 		for range ticker.C {
 			printCalculationSummary()
 		}
@@ -82,16 +83,16 @@ func main(){
 
 		// Handle the case when the computation has ended,
 		// we can end the program(exit out of this infinite loop)
-		case <- computationDone:
+		case <-computationDone:
 			ticker.Stop()
 			fmt.Println("Program done calculating Pi.")
-		    os.Exit(0)
+			os.Exit(0)
 
 		// If the user interrupts the program, we end the loop
 		case <-interrupt:
 			ticker.Stop()
 			fmt.Println("Program interrupted by the user.")
-		    return
+			return
 
 		}
 	}
