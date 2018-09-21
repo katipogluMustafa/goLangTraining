8a94ae0f46afb34a67051bf26d771432cd770d66
Mustafa Katipoğlu <mustafakatipoglu98@hotmail.com>
2018-09-21 23:18:31 +0300

    add API code decoupling examples

---
+++ 05_Composition/02_Decoupling/06/main.go
@@ -0,0 +1,147 @@
+/* Sample program demonstrating struct composition. */
+
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// Data is the structure of the data we are copying.
+type Data struct {
+	Line string
+}
+
+// Xenia is a system we need to pull data from.
+type Xenia struct {
+	Host    string
+	Timeout time.Duration
+}
+
+// Pull knows how to pull data out of Xenia
+func (*Xenia) Pull(d *Data) error {
+	switch rand.Intn(10) {
+	case 1, 9:
+		return io.EOF
+	case 5:
+		return errors.New("Error reading data from Xenia")
+	default:
+		d.Line = "Data"
+		fmt.Println("In:", d.Line)
+		return nil
+	}
+}
+
+// Pillar is a system we need to store data into.
+type Pillar struct {
+	Host    string
+	Timeout time.Duration
+}
+
+// Store knows how to store data into Pillar
+func (*Pillar) Store(d *Data) error {
+	fmt.Println("Out:", d.Line)
+	return nil
+}
+
+/* Deprecated Code since it hides the underlying costs and have some possibilities of potential misuse and fraud
+
+// System wraps Xenia and Pillar together into a single system.
+type System struct {
+	Puller
+	Storer
+}
+
+*/
+
+/* *************************************** */
+// Puller declares behavior for pulling data.
+type Puller interface {
+	Pull(d *Data) error
+}
+
+// Storer declares behavior for storing data.
+type Storer interface {
+	Store(d *Data) error
+}
+
+/* Deprecated interface
+
+type PullStorer interface {
+	Puller
+	Storer
+}
+
+*/
+/* *************************************** */
+
+// pull knows how to pull bulks of data from Xenia
+func pull(x Puller, data []Data) (int, error) {
+	for i := range data {
+		if err := x.Pull(&data[i]); err != nil {
+			return i, err
+		}
+	}
+
+	return len(data), nil
+}
+
+// store knows how to store bulks of data into Pillar
+func store(p Storer, data []Data) (int, error) {
+	for i := range data {
+		if err := p.Store(&data[i]); err != nil {
+			return i, err
+		}
+	}
+
+	return len(data), nil
+}
+
+// We've changed Copy functions signature so that
+// we don't hide any underlying cost and
+// block potential fraud and misuse
+
+func Copy(p Puller, s Storer, batch int) error {
+	data := make([]Data, batch)
+
+	for {
+		i, err := pull(p, data)
+		if i > 0 {
+			if _, err := store(s, data[:i]); err != nil {
+				return err
+			}
+		}
+
+		if err != nil {
+			return err
+		}
+	}
+}
+
+func main() {
+
+	x := Xenia{
+		Host:    "localhost:8000",
+		Timeout: time.Second,
+	}
+
+	p := Pillar{
+		Host:    "localhost:9000",
+		Timeout: time.Second,
+	}
+
+	if err := Copy(&x, &p, 3); err != io.EOF {
+		fmt.Println(err)
+	}
+}
+
+/*
+ * If you are not refactoring your code, you're not improving.
+ */
